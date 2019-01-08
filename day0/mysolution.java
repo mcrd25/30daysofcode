6f267b781d068e77c18f10abeb95551f6bb76732
mcrd25 <mayacrd25@gmail.com>
2019-01-08 00:35:45 -0400

    day 0 java, slight error fix

--- day0/mysolution.java
+++ day0/mysolution.java
@@ -17,7 +17,7 @@ public class Solution {
       
 		// Print a string literal saying "Hello, World." to stdout.
 		System.out.println("Hello, World.");
-        System.out.println("Welcome to 30 Days of Code!");
+        System.out.println(inputString);
       
 	    // TODO: Write a line of code here that prints the contents of inputString to stdout.
 	}
